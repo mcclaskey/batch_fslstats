1a24f85d93d7eb8e296f9a136b61c79176b36d48
Carolyn McClaskey <carolyn.mcclaskey@gmail.com>
2025-04-09 10:35:59 -0400

    streamlining echo commands
    This change adds more informative command line reporting and cleans up
    the echo commands to be more concise.
    
    Signed-off-by: Carolyn McClaskey <carolyn.mcclaskey@gmail.com>

--- run_fslstats.sh
+++ run_fslstats.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Define the CSV file name
+echo -e "\nRunning run_fslstats.sh\n"
 echo "Enter the full path, including the extension, of the input csv datalist"
 echo "(recall: must have input_file as column header), use tab for completion"
 echo "and do not use the ~ shortcut for home directory: "
@@ -11,11 +12,7 @@ if [[ ! -f "$datalist_fullfile" ]]; then
   echo "datalist not found!"
   exit 1
 fi
-
-echo ""
-echo "running FSLstats -M command on files listed in datalist: $datalist_fullfile"
-echo ""
-echo ""
+echo -e "\nrunning fslstats -M on files listed: $datalist_fullfile"
 
 # Read the CSV file line by line
 while IFS=, read -r input_file || [ -n "$input_file" ]; do
@@ -43,3 +40,6 @@ while IFS=, read -r input_file || [ -n "$input_file" ]; do
   eval $fslcmd
 
 done < "$datalist_fullfile"
+
+echo -e "\nProgram complete. Output text files are saved with the same"
+echo -e "filename as the original .nii, but with the suffix '*_mean.txt'\n"
