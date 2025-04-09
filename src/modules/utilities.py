b295d43fabd0fd2e139780ac50556320ac04a68d
Carolyn McClaskey <carolyn.mcclaskey@gmail.com>
2025-04-09 16:43:02 -0400

    creates subfunction to save df to same dir as input .csv
    This change calls a subfunction to saves the df to a file, and does so
    in the same input directory and with same basname as the input csv
    
    Signed-off-by: Carolyn McClaskey <carolyn.mcclaskey@gmail.com>

--- src/modules/utilities.py
+++ src/modules/utilities.py
@@ -9,14 +9,19 @@
 import tkinter as tk
 from tkinter import filedialog
 import src.modules.fsl as fsl
+import pandas as pd
+import datetime
+import os
 
 def askfordatalist(*args) -> str:
   """Asks user for data list file
 
+  first row must say "input_file" and rest must be list of files
+
   """
   root = tk.Tk()
   root.withdraw()
-  datalist_filepath = filedialog.askopenfilename() #first row must say "input_file" and rest must be list of files
+  datalist_filepath = filedialog.askopenfilename()
   return datalist_filepath
 
 
@@ -34,3 +39,22 @@ def compute_mean(nii_file: list[str],valid_files: list[str]) -> dict[str, float]
     else:
         print(f"File not found: {nii_file}")
         return None
+    
+
+def save_output_csv(output_df: pd.DataFrame, datalist_filepath: str):
+    """Saves data to csv file in same directory as input, with timestamp
+    
+    """
+    
+    # get output dir
+    output_dir = os.path.dirname(datalist_filepath)
+    
+	# get output filename
+    timestamp_file = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    datalist_filename = os.path.basename(datalist_filepath)
+    output_filename = f"{timestamp_file}_{datalist_filename.replace('.csv','_compiled.csv')}"
+    
+	# save to file
+    output_csv_fullfile = os.path.join(output_dir,output_filename)
+    output_df.to_csv(output_csv_fullfile, index=False)
+    print(f"\nOutput saved to file:\n{output_csv_fullfile}\n")
